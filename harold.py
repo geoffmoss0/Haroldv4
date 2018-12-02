9f88dc301b59582dc64fb81c17a1a0dfaf22ea41
Geoffrey Moss <geoffrey.moss@wheelergmail.org>
2018-12-01 23:43:46 -0500

    Everything's broke and I'm dying

--- harold.py
+++ harold.py
@@ -2,6 +2,7 @@ import time
 import random
 import os
 from pygame import mixer
+import vlc
 
 
 def weekday() -> bool:
@@ -18,22 +19,12 @@ class Harold:
 
     def run(self):
         if not self.playing:
-            mixer.init()
-            mixer.music.load(self.music)
-            mixer.music.play()
-            time.sleep(10)
+            pass
+            # mixer.init()
+            # mixer.music.load(self.music)
+            # mixer.music.play()
 
 
 def get_music():
     directory = "C://Users/gb_mo/Documents/Work/Coding/Haroldv4/songs"
     return os.path.join(directory, random.choice(os.listdir(directory)))
-
-
-def main():
-    h = Harold()
-    print(h.music)
-    h.run()
-
-
-if __name__ == "__main__":
-    main()
--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from sqlite3 import Error
 from sqlite3 import Connection
 import argparse
 import os
-from pygame import mixer
 from harold import Harold
-
 
 def create_conn():
     try:
@@ -16,28 +14,37 @@ def create_conn():
     return None
 
 
-def create_users(conn :Connection):
-    sql = """CREATE TABLE IF NOT EXISTS Users (
+def create_users(conn: Connection):
+    sql = """
+            CREATE TABLE IF NOT EXISTS users (
                 username text NOT NULL PRIMARY KEY,
                 path text,
                 song_plays integer
             );"""
     c = conn.cursor()
+    c.execute("DROP TABLE users")
     c.execute(sql)
 
 
 def main():
-    # mixer.init()
-    # mixer.music.load("C:\\Users\\gb_mo\\Downloads\\LandOfRuins.mp3");
-    # mixer.music.play()
 
     parser = argparse.ArgumentParser(description="Process command line variables")
     parser.add_argument("--debug", "-d", action="store_true")
 
     conn = create_conn()
+    c = conn.cursor()
     create_users(conn)
     h = Harold()
     h.run()
+
+    run = True
+    while run:
+        user_in = input("Input username")
+        if user_in == "quit":
+            run = False
+        else:
+            args = user_in.split(" ")
+            c.execute("""INSERT INTO Users(username, path, song_plays) VALUES( {username}, )""".format(username=args[0]))
 
 
 if __name__ == "__main__":
